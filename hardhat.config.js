c17d97de661ff8cd9545ff78ebe2ecb7f80fcfab
eibakatsu <eiba.katsu@gmail.com>
2022-09-20 00:00:03 +0900

    deployからverifyまで

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,8 @@
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config({path:__dirname+'/.env'})
-
 require("@nomiclabs/hardhat-waffle");
-// require("dotenv").config();
-// const dotenv = require("dotenv");
-// dotenv.config({path: __dirname + '/.env'});
 
 const {ALCHEMY_KEY, PRIVATE_KEY} = process.env;
-console.log("ALCHEMY_KEY:", ALCHEMY_KEY);
-// console.log("process.env:", process.env);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -19,4 +14,8 @@ module.exports = {
       accounts: [`${PRIVATE_KEY}`],
     },
   },
+
+  etherscan: {
+    apiKey: "W66I7MNM355YCSUDT9G7XVHGE7GX5QSNX5"
+  }
 };
