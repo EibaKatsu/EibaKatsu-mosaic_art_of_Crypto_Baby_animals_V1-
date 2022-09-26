cb4685fd6d4a4f2d38d5aa383a5556d70223e3f7
eibakatsu <eiba.katsu@gmail.com>
2022-09-26 17:19:24 +0900

    テストネットをgoerliに変更

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,24 @@
+// require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
-require('dotenv').config({path:__dirname+'/.env'})
-require("@nomiclabs/hardhat-waffle");
+// require('dotenv').config({path:__dirname+'/.env'})
+require('dotenv').config()
+// require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-chai-matchers");
 
-const {ALCHEMY_KEY, PRIVATE_KEY} = process.env;
+
+const {ALCHEMY_KEY_RINKEBY, ALCHEMY_KEY_GOERLI, PRIVATE_KEY, PRIVATE_KEY2} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.4",
 
   networks: {
+    goerli: {
+      url: "https://eth-goerli.alchemyapi.io/v2/" + ALCHEMY_KEY_GOERLI,
+      accounts: [`${PRIVATE_KEY2}`],
+    },
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/" + ALCHEMY_KEY,
+      url: "https://eth-rinkeby.alchemyapi.io/v2/" + ALCHEMY_KEY_RINKEBY,
       accounts: [`${PRIVATE_KEY}`],
     },
   },
